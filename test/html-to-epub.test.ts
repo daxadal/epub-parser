6a2bf29be3f4e6a3e9002f3ccd55b9ec7b5ceac2
Eric García de Ceca <e.garciadececa@gmail.com>
2024-04-11 16:12:26 +0200

    chore: fix test syntax

--- test/html-to-epub.test.ts
+++ test/html-to-epub.test.ts
@@ -1,13 +1,13 @@
 import assert from "assert";
 import { readFileSync } from "fs";
 import { resolve } from "path";
-import { fileURLToPath } from "url";
-import { EPub, EpubOptions } from "../src/index.js";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+import { EPub, EpubOptions } from "../src/html-to-epub";
 
 async function runTestOn(input: string): Promise<boolean> {
-  const params = JSON.parse(readFileSync(resolve(__dirname, `./${input}.json`), { encoding: "utf8" })) as EpubOptions;
+  const params = JSON.parse(
+    readFileSync(resolve(__dirname, `./${input}.json`), { encoding: "utf8" })
+  ) as EpubOptions;
   const output = resolve(__dirname, `./${input}.epub`);
 
   const epub = new EPub(params, output);
@@ -17,16 +17,16 @@ async function runTestOn(input: string): Promise<boolean> {
 
 it("Ebook > generate v2", async () => {
   assert.strictEqual(await runTestOn("book-v2"), true);
-}).timeout(8000);
+});
 
 it("Ebook > generate v3", async () => {
   assert.strictEqual(await runTestOn("book-v3"), true);
-}).timeout(8000);
+});
 
 it("HTML Page > generate v2", async () => {
   assert.strictEqual(await runTestOn("article-v2"), true);
-}).timeout(8000);
+});
 
 it("HTML Page > generate v3", async () => {
   assert.strictEqual(await runTestOn("article-v3"), true);
-}).timeout(8000);
+});
